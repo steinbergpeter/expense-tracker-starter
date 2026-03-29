5f016119d30b0f9d9bb90247ce9a6db5cc6755a7
Peter Steinberg <bklynpeter@gmail.com>
2026-03-29 11:02:50 -0400

    add exclamation to "Finance Tracker!" title on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <div className="app">
-      <h1>Finance Tracker</h1>
+      <h1>Finance Tracker!</h1>
       <p className="subtitle">Track your income and expenses</p>
 
       <Summary transactions={transactions} />
